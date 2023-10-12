c38c3af58f82366d506f85a198f65a04a095a382
afrojani <sultana.ani2019@gmail.com>
2023-10-12 02:16:11 -0500

    insertion at doubly linked list

--- Linked List/DoublyLinkedList.cpp
+++ Linked List/DoublyLinkedList.cpp
@@ -44,6 +44,33 @@ public:
         head = newnode;
     }
 
+    void Insert(int index, int value)
+    {
+        if (index > sz)
+        {
+            return;
+        }
+        if (index == 0)
+        {
+            InsertAtHead(value);
+            return;
+        }
+        node *a = head;
+        int cur_idx = 0;
+        while (cur_idx != index - 1)
+        {
+            a = a->nxt;
+            cur_idx++;
+        }
+        node *newnode = CreateNewNode(value);
+        newnode->nxt = a->nxt;
+        newnode->prv = a;
+        node *b = a->nxt;
+        b->prv = newnode;
+        a->nxt = newnode;
+        sz++;
+    }
+
     void Traverse()
     {
         node *a = head;
@@ -67,6 +94,8 @@ int main()
     dl.InsertAtHead(10);
     dl.InsertAtHead(7);
     dl.InsertAtHead(3);
+    dl.Traverse();
+    dl.Insert(1, 99);
     dl.Traverse();
     cout << dl.getSize() << "\n";
 
